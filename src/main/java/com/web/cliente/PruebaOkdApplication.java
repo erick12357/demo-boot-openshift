78db6bdbda8b7c901cdb8f80604aa35410e80612
Heri <heri@localhost.localdomain>
2020-10-04 22:50:16 -0500

    proyecto inicial

---
+++ src/main/java/com/web/cliente/PruebaOkdApplication.java
@@ -0,0 +1,26 @@
+package com.web.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PruebaOkdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaOkdApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hola() {
+		return "hola";
+	}
+	
+	@GetMapping("/saludo/{nombre}")
+	public String saludo() {
+		return "hola ";
+	}
+
+}
